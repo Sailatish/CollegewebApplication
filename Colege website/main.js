c8f0a657be4c734fc8b13479a2f0a25cb490c7c6
M. Sailatish <71929913+Sailatish@users.noreply.github.com>
2022-03-13 18:10:24 +0530

    Add files via upload

---
+++ Colege website/main.js
@@ -0,0 +1,63 @@
+var animation = bodymovin.loadAnimation({
+  container: document.getElementById('animContainer1'),
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: 'https://assets3.lottiefiles.com/private_files/lf30_htijkvxe.json' // lottie file path
+})
+var animation = bodymovin.loadAnimation({
+  container: document.getElementById('animContainer2'),
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: 'https://assets10.lottiefiles.com/packages/lf20_1pxqjqps.json' // lottie file path
+})
+var animation = bodymovin.loadAnimation({
+  container: document.getElementById('animContainer3'),
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: 'https://assets9.lottiefiles.com/private_files/lf30_JenNba.json' // lottie file path
+})
+var animation = bodymovin.loadAnimation({
+  container: document.getElementById('animContainer4'),
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: 'https://assets4.lottiefiles.com/private_files/lf30_9142zhsb.json' // lottie file path
+})
+var animation = bodymovin.loadAnimation({
+  container: document.getElementById('animContainer5'),
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: 'https://assets4.lottiefiles.com/packages/lf20_kmabq1m3.json' // lottie file path
+})
+var animation = bodymovin.loadAnimation({
+  container: document.getElementById('animContainer6'),
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: 'gallery.json' // lottie file path
+})
+var animation = bodymovin.loadAnimation({
+  container: document.getElementById('animContainer7'),
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: 'https://assets6.lottiefiles.com/packages/lf20_n2yhd0lo.json' // lottie file path
+})
+var animation = bodymovin.loadAnimation({
+  container: document.getElementById('animContainer8'),
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: 'https://assets1.lottiefiles.com/packages/lf20_ei2gf306.json' // lottie file path
+})
+var animation = bodymovin.loadAnimation({
+  container: document.getElementById('animContainer9'),
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: 'https://assets10.lottiefiles.com/packages/lf20_1pxqjqps.json' // lottie file path
+})
